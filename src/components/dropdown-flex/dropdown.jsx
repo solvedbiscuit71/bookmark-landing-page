81c9049180fcf4359b1fec2df50bd09514335497
Praveen Perumal <ppraveen98841@gmail.com>
2022-03-24 18:45:27 +0530

    completed desktop layout

--- src/components/dropdown-flex/dropdown.jsx
+++ src/components/dropdown-flex/dropdown.jsx
@@ -4,9 +4,9 @@ function DropDown(props) {
   const [show,setShow] = useState(false)
 
   return (
-    <div className={`dropdown ${show ? "dropdown--show" : ""}`}>
+    <div onClick={() => setShow(show ? false : true)} className={`dropdown ${show ? "dropdown--show" : ""}`}>
       <h4>{props.title}</h4>
-      <svg onClick={() => setShow(show ? false : true)} xmlns="http://www.w3.org/2000/svg" width="18" height="12" className="arrow"><path fill="none" stroke="#5267DF" strokeWidth="3" d="M1 1l8 8 8-8"/></svg>
+      <svg xmlns="http://www.w3.org/2000/svg" width="18" height="12" className="arrow"><path fill="none" stroke="#5267DF" strokeWidth="3" d="M1 1l8 8 8-8"/></svg>
 
       <p>{props.para}</p>
     </div>
