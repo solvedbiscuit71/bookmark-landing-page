54de5bd0572ef74fa88111515b33c3fe12d670f5
Praveen Perumal <ppraveen98841@gmail.com>
2022-03-24 19:02:15 +0530

    Resolve accessibility issue 1
    Heading levels should only increase by one
    
    Context:
    ```
    <h4>What is Bookmark?</h4>
    ```

--- src/components/dropdown-flex/dropdown.jsx
+++ src/components/dropdown-flex/dropdown.jsx
@@ -5,7 +5,7 @@ function DropDown(props) {
 
   return (
     <div onClick={() => setShow(show ? false : true)} className={`dropdown ${show ? "dropdown--show" : ""}`}>
-      <h4>{props.title}</h4>
+      <h3>{props.title}</h3>
       <svg xmlns="http://www.w3.org/2000/svg" width="18" height="12" className="arrow"><path fill="none" stroke="#5267DF" strokeWidth="3" d="M1 1l8 8 8-8"/></svg>
 
       <p>{props.para}</p>
