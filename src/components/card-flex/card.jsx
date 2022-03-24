524a81ae5132cf4c7623ee3823ae3aae310308a5
Praveen Perumal <ppraveen98841@gmail.com>
2022-03-24 09:56:27 +0530

    completed styling the card component

--- src/components/card-flex/card.jsx
+++ src/components/card-flex/card.jsx
@@ -2,7 +2,7 @@ function Card({ name,version }) {
   return (
     <div className="card">
       <img src={`images/logo-${name}.svg`} alt={`${name} logo`} />
-      <h2>Add to {name}</h2>
+      <h3>Add to {name}</h3>
       <p>Minimum version {version}</p>
 
       <button className="btn btn--primary">Add & Install Extension</button>
