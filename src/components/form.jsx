8c3e7242e05c28e08d6ce478a96f31e9b4797484
Praveen Perumal <ppraveen98841@gmail.com>
2022-03-24 16:36:06 +0530

    added email validator in <Form> component

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,14 +1,29 @@
+import React, { useState } from 'react';
+
 function Form() {
+  const [error,setError] = useState(false)
+  const [email,setEmail] = useState("")
+
+  const checkEmail = (event) => {
+    event.preventDefault()
+    const validator = /^[a-zA-Z0-9#$^&*{}?-]+(\.[a-zA-Z0-9#$^&*{}?-]+)*@[a-zA-Z0-9#$^&*{}?-]+(\.[a-zA-Z0-9#$^&*{}?-]+)*\.[a-z]+$/
+    if (!validator.test(email)) {
+      setError(true)
+      return
+    }
+    setError(false)
+  }
+
   return (
     <form>
-      <div className="input">
-        <input type="text" placeholder="Enter your email address" />
+      <div className={`input ${error ? "input--error" : ""}`}>
+        <input onInput={event => setEmail(event.target.value)} type="text" placeholder="Enter your email address" />
 
         <img src="images/icon-error.svg" alt="error icon" />
         <span>Whoops, make sure it's an email</span>
       </div>
 
-      <button className="btn btn--secondary">Contact Us</button>
+      <button onClick={checkEmail} className="btn btn--secondary">Contact Us</button>
     </form>
   );
 }
