1ead7b2ab652a27bf2505e5cb5cd22b0398176ce
Praveen Perumal <ppraveen98841@gmail.com>
2022-03-24 10:31:56 +0530

    added tab & dropdown functionality

--- src/components/dropdown-flex/dropdown.jsx
+++ src/components/dropdown-flex/dropdown.jsx
@@ -1,8 +1,12 @@
+import React, { useState } from 'react';
+
 function DropDown(props) {
+  const [show,setShow] = useState(false)
+
   return (
-    <div className="dropdown">
+    <div className={`dropdown ${show ? "dropdown--show" : ""}`}>
       <h4>{props.title}</h4>
-      <svg xmlns="http://www.w3.org/2000/svg" width="18" height="12" className="arrow"><path fill="none" stroke="#5267DF" strokeWidth="3" d="M1 1l8 8 8-8"/></svg>
+      <svg onClick={() => setShow(show ? false : true)} xmlns="http://www.w3.org/2000/svg" width="18" height="12" className="arrow"><path fill="none" stroke="#5267DF" strokeWidth="3" d="M1 1l8 8 8-8"/></svg>
 
       <p>{props.para}</p>
     </div>
--- src/components/tab.jsx
+++ src/components/tab.jsx
@@ -1,21 +1,45 @@
+import React, { useState } from 'react';
+
 function Tab() {
+  const data = [
+    {
+      name: "Simple Bookmarking",
+      title: "Bookmark in one click",
+      para: `
+        Organize your bookmarks however you like. Our simple drag-and-drop interface 
+        gives you complete control over how you manage your favourite sites.
+      `
+    },
+    {
+      name: "Speedy Searching",
+      title: "Intelligent search",
+      para: `
+        Our powerful search feature will help you find saved sites in no time at all. 
+        No need to trawl through all of your bookmarks.
+      `
+    },
+    {
+      name: "Easy Sharing",
+      title: "Share your bookmarks",
+      para: `
+        Easily share your bookmarks and collections with others. Create a shareable 
+        link that you can send at the click of a button.
+      `
+    }
+  ]
+  const [cur,setCur] = useState(0)
+
   return (
     <div className="tab">
       <ul className="tab-names">
-        <li><div className="tab-name tab-name--active">Simple Bookmarking</div></li>
-        <li><div className="tab-name">Speedy Searching</div></li>
-        <li><div className="tab-name">Easy Sharing</div></li>
+        { data.map(( tab,index ) => <li><div onClick={() => setCur(index)} className={`tab-name ${index === cur ? "tab-name--active" : ""}`}>{tab.name}</div></li>)}
       </ul>
 
       <div className="tab-content">
-        <img src="images/illustration-features-tab-1.svg" alt="illustration image for features tab" />
+        <img src={`images/illustration-features-tab-${cur + 1}.svg`}alt="illustration image for features tab" />
 
-        <h2>Bookmark in one click</h2>
-        <p>
-          Organize your bookmarks however you like. Our simple drag-and-drop interface 
-          gives you complete control over how you manage your favourite sites.
-        </p>
-
+        <h2>{data[cur].title}</h2>
+        <p>{data[cur].para}</p>
         <button className="btn btn--primary">More Info</button>
       </div>
     </div>
