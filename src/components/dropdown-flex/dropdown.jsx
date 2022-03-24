11e584ad92285175d165efe593d28a6c5613529d
Praveen Perumal <ppraveen98841@gmail.com>
2022-03-24 10:18:50 +0530

    completed styling dropdown component

--- src/components/dropdown-flex/dropdown.jsx
+++ src/components/dropdown-flex/dropdown.jsx
@@ -1,8 +1,8 @@
 function DropDown(props) {
   return (
     <div className="dropdown">
-      <h2>{props.title}</h2>
-      <img src="images/icon-arrow.svg" alt="dropdown arrow" className="arrow arrow--active" />
+      <h4>{props.title}</h4>
+      <svg xmlns="http://www.w3.org/2000/svg" width="18" height="12" className="arrow"><path fill="none" stroke="#5267DF" strokeWidth="3" d="M1 1l8 8 8-8"/></svg>
 
       <p>{props.para}</p>
     </div>
